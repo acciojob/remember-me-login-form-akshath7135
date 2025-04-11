7308206eb7a2d5ab8103b9f25286bc55f07e7f6e
akshath7135 <akshathabrol21122004@gmail.com>
2025-04-11 14:35:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 document.addEventListener("DOMContentLoaded", function () {
     const usernameInput = document.getElementById("username");
     const passwordInput = document.getElementById("password");
